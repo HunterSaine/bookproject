6639797e30166cb317bd7f9f086269294641a463
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-13 00:28:09 -0500

    added index and keys

--- retrieve.py
+++ retrieve.py
@@ -1,8 +1,12 @@
 import requests
+import json
 
 apikey = 'e9be841a2b734322476b32827df51230'
 
-response = requests.get("https://api.the-odds-api.com/v4/sports/?apiKey=e9be841a2b734322476b32827df51230")
-
-o = response.json()
-
+response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=e9be841a2b734322476b32827df51230&regions=us&markets=h2h,spreads&oddsFormat=american")
+o = response.json() 
+firstkey = o[1]
+k = (firstkey["bookmakers"])
+print(k)
+h = k[1]
+print(h["key"]) 
