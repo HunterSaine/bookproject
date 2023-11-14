fccf2cf5dd6aecf4bc43ef9e21a8dc10cc1a6c34
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-14 16:25:04 -0500

    removed api loop

--- retrieve.py
+++ retrieve.py
@@ -5,8 +5,10 @@ apikey = 'e9be841a2b734322476b32827df51230'
 
 response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=e9be841a2b734322476b32827df51230&regions=us&markets=h2h,spreads&oddsFormat=american")
 o = response.json() 
-firstkey = o[1]
-k = (firstkey["bookmakers"])
-print(k)
-h = k[1]
-print(h["key"]) 
+
+for i in o:
+    print({id})
+
+
+
+
