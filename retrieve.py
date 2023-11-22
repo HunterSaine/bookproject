7758f97212c14a42f883d4e5c7fa66f40b8cf0a7
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-22 00:00:22 -0500

    cleaned up comments and unused code

--- retrieve.py
+++ retrieve.py
@@ -2,22 +2,15 @@ import requests
 import json
 
 apikey = 'e9be841a2b734322476b32827df51230'
-
 response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=e9be841a2b734322476b32827df51230&regions=us&markets=h2h,spreads&oddsFormat=american")
 
-
 json_data = response.json() if response and response.status_code == 200 else None
-
 formatted_json = json.dumps(json_data, indent=2) #format json response
+#force quit if out of api responses
 if len(formatted_json) < 5:     
     print("Out of API request. Please try again later")
     quit()
 
-
-
-#f = open("sportslines.txt", "a")
-#f.write(formatted_json)
-#f.close()
 fanduel_info = ""
 fanduel_h2h = ""
 draftkings_info = ""
@@ -35,22 +28,21 @@ while i < len(json_data):
                 draftkings_info = json_data[i]["bookmakers"][j]["markets"]
             j += 1
     i += 1
-#print(fanduel_info)
 try:
+#fanduel variables
     fanduel_h2h = fanduel_info[0]
     fanduel_h2h_team1 = fanduel_h2h["outcomes"][0]
     fanduel_h2h_team2 = fanduel_h2h["outcomes"][1]
     fanduel_spread = fanduel_info[1]
     fanduel_spread_team1 = fanduel_spread["outcomes"][0]
     fanduel_spread_team2 = fanduel_spread["outcomes"][1]
-
+#draftkings variables
     draftkings_h2h = draftkings_info[0]
     draftkings_h2h_team1 = draftkings_h2h["outcomes"][0]
     draftkings_h2h_team2 = draftkings_h2h["outcomes"][1]
     draftkings_spread = draftkings_info[1]
     draftkings_spread_team1 = draftkings_spread["outcomes"][0]
     draftkings_spread_team2 = draftkings_spread["outcomes"][1]
-#print(fanduel_h2h_team1)
     if fanduel_h2h_team1["price"] >= 0:
         print("Fanduel Odds:")
         print(f'{fanduel_h2h_team1['name']} +{fanduel_h2h_team1['price']} ')
@@ -58,19 +50,13 @@ try:
         print(f'Spread: {fanduel_spread_team1["name"]} +{fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}')
         print(f'Spread: {fanduel_spread_team2["name"]} {fanduel_spread_team2["point"]} {fanduel_spread_team2['price']}')
         print('')
-
     else:
         print("Fanduel Odds")
         print(f'{fanduel_h2h_team1['name']} {fanduel_h2h_team1['price']} ')
         print(f'{fanduel_h2h_team2['name']} +{fanduel_h2h_team2['price']} ')
-        print(f'{fanduel_spread_team1["name"]} +{fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}')
-
-    
+        print(f'{fanduel_spread_team1["name"]} +{fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}')    
 except:
     print("Error: Enter valid team. Format ex. Miami Dolphins")
-
-
-
 
 i = 0
 while i < len(json_data):
@@ -84,32 +70,3 @@ while i < len(json_data):
                 draftkings_info = json_data[i]["bookmakers"][j]["markets"]
             j += 1
     i += 1
-
-#print(fanduel_info)
-
-#print(json_data[0]["bookmakers"][0]["key"])
-#i = 0
-#while i < 15:
-#    k = o[i]
-#    j = k["bookmakers"]
-#    l = (j[1])
-#    z = 0
-#    while z < 1:
-#        l = (j[z])
-#        m = l["markets"]
-#        h2h = m[0]
-#        spread = m[1]
-#        ind = h2h["outcomes"]
-#        sp = spread["outcomes"]
-#        team_one_spread = sp[0]
-#        team_two_spread = sp[1]
-#        
-#        team_one_h2h = ind[0]
-#        team_two_h2h = ind[1]
-#        f = open("sportslines.txt")
-#        #f.write(i,z)
-#    f.write(f"Spread: {team_one_spread["name"]} {team_one_spread["price"]} {team_one_spread["point"]} vs {team_two_spread["name"]} {team_two_spread["price"]} {team_two_spread["point"]}")
-#
-#        f.write()
-#        z += 1
-#    i += 1
