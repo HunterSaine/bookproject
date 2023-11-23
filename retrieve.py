37e563154511e2f30a24f8306bd4cc5355dd1add
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-23 15:42:01 -0500

    included betting lines from unibet

--- retrieve.py
+++ retrieve.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+teamselect = input("Which team would you like info on? ")
+
 apikey = 'e9be841a2b734322476b32827df51230'
 apikeywgu ="b0deb1a32f7c45cb9785d4d1405b6a5a"
 response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=b0deb1a32f7c45cb9785d4d1405b6a5a&regions=us&markets=h2h,spreads&oddsFormat=american")
@@ -15,7 +17,7 @@ if len(formatted_json) < 5:
 fanduel_info = ""
 fanduel_h2h = ""
 draftkings_info = ""
-teamselect = input("Which team would you like info on? ")
+unibet_info = ''
 print()
 i = 0 
 while i < len(json_data):
@@ -27,6 +29,8 @@ while i < len(json_data):
                 fanduel_info = json_data[i]["bookmakers"][j]["markets"]
             elif json_data[i]["bookmakers"][j]['key'] == "draftkings":
                 draftkings_info = json_data[i]["bookmakers"][j]["markets"]
+            elif json_data[i]["bookmakers"][j]['key'] == "unibet_us":
+                unibet_info = json_data[i]["bookmakers"][j]["markets"]
             j += 1
     elif json_data[i]["away_team"] == teamselect :
         j = 0 
@@ -36,6 +40,9 @@ while i < len(json_data):
                 fanduel_info = json_data[i]["bookmakers"][j]["markets"]
             elif json_data[i]["bookmakers"][j]['key'] == "draftkings":
                 draftkings_info = json_data[i]["bookmakers"][j]["markets"]
+            elif json_data[i]["bookmakers"][j]['key'] == "unibet_us":
+                unibet_info = json_data[i]["bookmakers"][j]["markets"]
+            
             j += 1
     i += 1
 try:
@@ -53,8 +60,16 @@ try:
     draftkings_spread = draftkings_info[1]
     draftkings_spread_team1 = draftkings_spread["outcomes"][0]
     draftkings_spread_team2 = draftkings_spread["outcomes"][1]
+#unibet variables
+    unibet_h2h = unibet_info[0]
+    unibet_h2h_team1 = unibet_h2h["outcomes"][0]
+    unibet_h2h_team2 = unibet_h2h["outcomes"][1]
+    unibet_spread = unibet_info[1]
+    unibet_spread_team1 = unibet_spread["outcomes"][0]
+    unibet_spread_team2 = unibet_spread["outcomes"][1]
     if fanduel_h2h_team1["price"] >= 0:
         print("Fanduel Odds:")
+        print("Moneyline: ")
         print(f'{fanduel_h2h_team1['name']} +{fanduel_h2h_team1['price']} ')
         print(f'{fanduel_h2h_team2['name']} {fanduel_h2h_team2['price']} ')
         print(f'Spread: {fanduel_spread_team1["name"]} +{fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}')
@@ -62,25 +77,46 @@ try:
         print()
     else:
         print("Fanduel Odds")
+        print("Moneyline: ")
         print(f'{fanduel_h2h_team1['name']} {fanduel_h2h_team1['price']} ')
         print(f'{fanduel_h2h_team2['name']} +{fanduel_h2h_team2['price']} ')
-        print(f'{fanduel_spread_team1["name"]} {fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}') 
-        print(f'{fanduel_spread_team2["name"]} +{fanduel_spread_team2["point"]} {fanduel_spread_team2['price']}') 
+        print(f'Spread: {fanduel_spread_team1["name"]} {fanduel_spread_team1["point"]} {fanduel_spread_team1['price']}') 
+        print(f'Spread: {fanduel_spread_team2["name"]} +{fanduel_spread_team2["point"]} {fanduel_spread_team2['price']}') 
 
         print()   
     if draftkings_h2h_team1["price"] >= 0 :
         print("DraftKings Odds:")
+        print("Moneyline: ")
         print(f'{draftkings_h2h_team1['name']} +{draftkings_h2h_team1['price']} ')
         print(f'{draftkings_h2h_team2['name']} {draftkings_h2h_team2['price']} ')
         print(f'Spread: {draftkings_spread_team1["name"]} +{draftkings_spread_team1["point"]} {draftkings_spread_team1['price']}')
         print(f'Spread: {draftkings_spread_team2["name"]} {draftkings_spread_team2["point"]} {draftkings_spread_team2['price']}')
         print('')
     else:
-        print("Draftkings Odss:")
+        print("Draftkings Odss:") 
+        print("Moneyline: ")
         print(f'{draftkings_h2h_team1['name']} {draftkings_h2h_team1['price']} ')
         print(f'{draftkings_h2h_team2['name']} +{draftkings_h2h_team2['price']} ')
         print(f'Spread: {draftkings_spread_team1["name"]} {draftkings_spread_team1["point"]} {draftkings_spread_team1['price']}') 
         print(f'Spread: {draftkings_spread_team2["name"]} +{draftkings_spread_team2["point"]} {draftkings_spread_team2['price']}')
+        print()
+    #unibet
+    if unibet_h2h_team1["price"] >= 0 :
+        print("Unibet Odds:")
+        print("Moneyline: ")
+        print(f'{unibet_h2h_team1['name']} +{unibet_h2h_team1['price']} ')
+        print(f'{unibet_h2h_team2['name']} {unibet_h2h_team2['price']} ')
+        print(f'Spread: {unibet_spread_team1["name"]} +{unibet_spread_team1["point"]} {unibet_spread_team1['price']}')
+        print(f'Spread: {unibet_spread_team2["name"]} {unibet_spread_team2["point"]} {unibet_spread_team2['price']}')
+        print('')
+    else:
+        print("Unibet Odss:") 
+        print("Moneyline: ")
+        print(f'{unibet_h2h_team1['name']} {unibet_h2h_team1['price']} ')
+        print(f'{unibet_h2h_team2['name']} +{unibet_h2h_team2['price']} ')
+        print(f'Spread: {unibet_spread_team1["name"]} {unibet_spread_team1["point"]} {unibet_spread_team1['price']}') 
+        print(f'Spread: {unibet_spread_team2["name"]} +{unibet_spread_team2["point"]} {unibet_spread_team2['price']}')
+        print()
 
 except:
     print("Error: Enter valid team. Format ex. Miami Dolphins")
@@ -97,5 +133,3 @@ while i < len(json_data):
                 draftkings_info = json_data[i]["bookmakers"][j]["markets"]
             j += 1
     i += 1
-
-    
